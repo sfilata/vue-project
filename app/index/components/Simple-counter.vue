8aeafe1036ec23d5337ece2f43f61fa7a71fd09d
sfilata <shubaiqiao@126.com>
2017-04-19 21:30:21 +0800

    测试vue

---
+++ app/index/components/Simple-counter.vue
@@ -0,0 +1,11 @@
+<template>
+	<button v-on:click="count += 1">{{ counter }}</button>
+</template>
+
+<script>
+    export default {
+        data () {
+            return data;
+        }
+    }
+</script>
--- app/index/index.js
+++ app/index/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import favlist from './components/Favlist.vue'
+import Vue from 'vue';
+import favlist from './components/Favlist.vue';
+import simpleCounter from './components/Simple-counter.vue';
 
-new Vue({
-	el: '#app',
-	render: h => h(favlist),
-	components: { favlist }
-})
-
-// var app = new Vue({
+// new Vue({
 // 	el: '#app',
-// 	data: {
-// 		message: 'hello world'
-// 	}
-// });
+// 	render: h => h(favlist)
+// })
+
+var app = new Vue({
+	el: '#app',
+	data: {
+		message: 'hello world'
+	}
+});
+
+var data = { counter: 0 }
+new Vue({
+  el: 'simple-counter',
+  render: h => h(simpleCounter)
+})
