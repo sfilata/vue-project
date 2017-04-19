ca8437ecab387656ab213ec6d70820b6bbafd38d
sfilata <shubaiqiao@126.com>
2017-04-19 20:44:31 +0800

    配置哈希值

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,5 +1,6 @@
 // nodejs 中的path模块
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     // 入口文件，path.resolve()方法，可以结合我们给定的两个参数最后生成绝对路径，最终指向的就是我们的index.js文件
@@ -31,5 +32,13 @@ module.exports = {
             	exclude: /node_modules/
             }
         ]
-    }
+    },
+
+    plugins: [
+    	new HtmlWebpackPlugin ({
+    		filename: '../index.html',
+    		template: path.resolve(__dirname, '../app/index/index.html'),
+    		inject: true
+    	})
+    ]
 }
