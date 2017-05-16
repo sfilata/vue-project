128466a216b21db6b467b9ee04363f61be85b5f6
sfilata <sfilata@hotmail.com>
2017-05-16 16:42:19 +0800

    开发过程中

--- app/index/components/Favlist.vue
+++ app/index/components/Favlist.vue
@@ -16,7 +16,5 @@
 </script>
 
 <style>
-    html {
-        background: green;
-    }
+
 </style>	
--- app/index/index.js
+++ app/index/index.js
@@ -1,7 +1,20 @@
 import Vue from 'vue';
 import favlist from './components/Favlist.vue';
+import login from './components/Login.vue';
+import header from './components/Header.vue';
 
-new Vue({
-	el: 'favlist',
-	render: h => h(favlist)
-})
+// new Vue({
+// 	el: 'favlist',
+// 	render: h => h(favlist)
+// })
+
+
+var loginTool = new Vue({
+	el: '#app',
+	render: h => h(login)
+});
+
+// var header = new Vue({
+// 	el: '.header',
+// 	render: h => h(header)
+// });
